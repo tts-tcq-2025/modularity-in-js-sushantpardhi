46e144312b9e15c5cffecf502a4839a34ba196dc
Sushant <sushantpardhi186@gmail.com>
2025-08-20 16:21:49 +0530

    Create colorPair.js

---
+++ app/colorPair.js
@@ -0,0 +1,20 @@
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+function ColorPair(){
+           this.majorColor;
+           this.minorColor;
+}
+
+ColorPair.prototype.toString=function(){
+	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
+}
+module.exports = {
+    ColorPair,
+    MajorColorNames,
+    MinorColorNames
+};
