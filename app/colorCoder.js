37e919fab9a78ddf633812357e13f09b959e82d7
Sushant <sushantpardhi186@gmail.com>
2025-08-20 16:22:22 +0530

    Update colorCoder.js

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -1,20 +1,3 @@
-
-const MajorColorNames = [
-    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
-];
-const MinorColorNames = [
-	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
-];
-
-function ColorPair(){
-           this.majorColor;
-           this.minorColor;
-}
-
-ColorPair.prototype.toString=function(){
-	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
-}
-
 function getColorFromPairNumber(pairNumber)
 {
 	let minorSize = MajorColorNames.length;
